4057d26d13b8143d49af796baf5b9706030fadb6
toogy <toogy@free.fr>
2012-11-17 14:06:43 +0100

    Finally

--- TP6/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ TP6/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -77,25 +78,28 @@
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.White;
             this.button6.Enabled = false;
             this.button6.Location = new System.Drawing.Point(24, 12);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(86, 37);
             this.button6.TabIndex = 5;
             this.button6.Text = "-";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(138, 160);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(135, 165);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "s";
             this.Load += new System.EventHandler(this.Form1_Load);
