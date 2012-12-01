5416851101a8a82bff95ab759f292069bbdedde1
toogy <contact@toogy.fr>
2012-12-01 14:28:59 +0100

    tp8

---
+++ TP8/Ex1/Ex1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allowedOps = new List<string> { "+", "-", "/", "*" };
+            List<float> nb = new List<float> { };
+            List<string> ops = new List<string> { };
+
+            #region table-genesis
+            
+            foreach (string a in args)
+            {
+                float x;
+                if (float.TryParse(a, out x))
+                    nb.Add(float.Parse(a));
+
+                else
+                {
+                    if (a.Length > 2 && a[0] == '+' && a[1] == '+' && float.TryParse(a.Substring(2), out x))
+                        nb.Add(x + 1);
+                    else
+                    {
+                        if (a.Length > 2 && a[0] == '-' && a[1] == '-' && float.TryParse(a.Substring(2), out x))
+                            nb.Add(x - 1);
+                        else
+                        {
+                            if (a.Length == 1 && allowedOps.Contains(a))
+                                ops.Add(a);
+                            else
+                            {
+                                Console.WriteLine("Argument invalide");
+                                Environment.Exit(0);
+                            }
+                        }
+                    }
+                }
+            }
+
+            #endregion
+
+            #region calculage-ou-calculation-hehe
+
+            // Si il n'y a pas d'operateur, on renvoit une erreur
+            if (ops.Intersect(allowedOps).Count() == 0)
+                Console.WriteLine("Pas d'operateur");
+
+            else
+            {
+                // On applique les multiplications d'abord
+                while (ops.Contains("*"))
+                {
+                    int i = ops.IndexOf("*");
+                    nb[i] = nb[i] * nb[i + 1];
+                    nb.RemoveAt(i + 1);
+                    ops.RemoveAt(i);
+                }
+
+                // Puis les divisions, en gerant bien evidemment les divisions par zero
+                while (ops.Contains("/"))
+                {
+                    int i = ops.IndexOf("/");
+
+                    if (nb[i + 1] == 0)
+                    {
+                        Console.WriteLine("...Petite erreur...");
+                        Console.WriteLine("Division par zero interdite, considerons que ca fait zero...");
+                        Console.WriteLine("...Le calcul continue");
+                        nb[i] = 0;
+                    }
+                    else
+                        nb[i] = nb[i] / (nb[i + 1]);
+
+                    nb.RemoveAt(i + 1);
+                    ops.RemoveAt(i);
+                }
+
+                // Puis les additions
+                while (ops.Contains("+"))
+                {
+                    int i = ops.IndexOf("+");
+                    nb[i] = nb[i] + (nb[i + 1]);
+                    nb.RemoveAt(i + 1);
+                    ops.RemoveAt(i);
+                }
+
+                // Puis les soustractions
+                while (ops.Contains("-"))
+                {
+                    int i = ops.IndexOf("-");
+                    nb[i] = nb[i] - (nb[i + 1]);
+                    nb.RemoveAt(i + 1);
+                    ops.RemoveAt(i);
+                }
+
+                // Et on ecrit le resultat de tout ca, tranquillement
+                Console.WriteLine(nb[0].ToString());
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ TP8/Ex3/Ex3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                Sapin(n);
+                System.Threading.Thread.Sleep(300);
+                Console.Clear();
+            }
+        }
+
+        static List<ConsoleColor> colorsList = new List<ConsoleColor> { ConsoleColor.White, ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.Blue };
+
+        static byte[] randomNumber = new byte[1];
+        static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+        static void Sapin(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.ForegroundColor = (i < 2) ? ConsoleColor.Yellow : ConsoleColor.Green;
+
+                for (int j = 0; j < (n-i); j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < (2*i-1); j++)
+                {
+                    rngCsp.GetBytes(randomNumber);
+                    if (j == 0 && i == 1)
+                        Console.Write("$");
+                    else
+                    {
+                        if (randomNumber[0] % 4 == 3)
+                        {
+                            rngCsp.GetBytes(randomNumber);
+                            Console.ForegroundColor = colorsList[randomNumber[0] % 6];
+                            Console.Write("o");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                string s = "";
+
+                for (int j = 0; j < ((2 * n - 1) / 2); j++)
+                {
+                    s += " ";
+                }
+
+                s += "#";
+
+                Console.WriteLine(s);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
